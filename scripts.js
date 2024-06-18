4b90eb14329234cb2c63ee82006d8ee481baafe3
Si5193 <simonstrandlund@gmail.com>
2024-06-18 16:43:41 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,33 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.getElementById('hamburger');
-    const navbar = document.getElementById('navbar');
-
-    hamburger.addEventListener('click', function() {
-        navbar.classList.toggle('show');
-    });
-
     // Scroll to section
     window.scrollToSection = function(sectionId) {
         const section = document.getElementById(sectionId);
-        section.scrollIntoView({ behavior: 'smooth' });
-    };
-
-    // Fade-in effect on scroll
-    const sections = document.querySelectorAll('.fade-in');
-    const options = {
-        threshold: 0.1
-    };
-
-    const observer = new IntersectionObserver(function(entries, observer) {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('appear');
-                observer.unobserve(entry.target);
-            }
-        });
-    }, options);
-
-    sections.forEach(section => {
-        observer.observe(section);
-    });
-});
+        section.scrollIntoView({ behavior: 'smooth
